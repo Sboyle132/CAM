6dca54d9b8f24e1a6782d681e66a56f9bb4a0106
Samuel Boyle <boyle@euporie.informatik.uni-kl.de>
2022-08-04 14:54:17 +0200

    Working on integrating into Cyclone V

--- design/svga_master.vhd
+++ design/svga_master.vhd
@@ -9,7 +9,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity svga_master is
 generic ( 
-			threshold : integer := 4
+			mclk_freq : integer := 25000000
 );
 port(
 	 -- FPGA internal
@@ -28,6 +28,118 @@ port(
 	 
     );
 end svga_master;
+
+
+
+
+architecture svga_master_arch of svga_master is
+
+
+
+
+component scl_generator is
+    generic ( 
+        threshold : integer
+    );
+    port (
+        rst : in std_logic;
+        clk : in std_logic;
+        toggle : in std_logic;
+        scl_gen : out std_logic;
+        scl_quarter : out std_logic_vector(1 downto 0)
+		  );
+end component;
+
+constant VSYNC_TIME; -- More than 1 line
+constant HSYNC_TIME; -- more than 6 cycles.
+constant VSYNC_PERIOD; -- 672 * Time of line
+constant HSYNC_PERIOD; -- Time of line
+
+TYPE State_type IS (IDLE, AKTIV);
+SIGNAL State : State_Type;
+signal enab : std_logic;
+signal VSYNC_COUNT std_logic_vector;
+signal HSYNC_COUNT std_logic_vector;
+signal mclk_toggle : std_logic;
+begin
+
+scl_generator_svga : scl_generator
+		generic map (
+				threshold => (50000000/mclk_freq)
+				)
+		port map( 
+            rst => rst_gen,
+            clk => clk,
+            scl_gen => sig_mclk,
+            toggle => mclk_toggle,
+            scl_quarter => mclk_quarter
+		);
+mclk <= sig_mclk;		
+		
+-- FSM controller process.
+process(rst, clk)
+begin
+	if	(rst='1') then
+		enable_change <= '1';
+		mclk_toggle <= '0';
+	elsif (clk'event and clk='1') then
+		  if(enable = '1') then
+            enable_change <= '0';
+        elsif (enable = '0') then
+            enable_change <= '1';
+        end if;
+		  
+		  if enable = '1' and enable_change = '1' then
+				if(STATE = AKTIV) then
+					STATE <= IDLE;
+					mclk_toggle <= '0';
+				elsif(STATE = IDLE) then
+					STATE <= AKTIV;
+					mclk_toggle <= '1';
+				else 
+					STATE <= STATE;
+					mclk_toggle <= '0';
+				end if;
+			else
+				mclk_toggle <= '0';
+			end if;
+	end if;
+end process;
+-- Put in, enable = '1' and (on rise to '1'.), then enable scl_generator_svga,
+-- Move into initial state (from initial to idle). Could have output/input manager and one for internal state.
+
+--May need some kind of counter.
+
+--MCLK, HREF, VSYNC CONTROLLER
+process(rst, clk)
+begin
+	if (rst='1') then
+		data_o <= (others => '0');
+		data_valid <= '0';
+		MHSYNC <= '0';
+		MVSYNC <= '0';
+		
+	elsif(clk'event and clk='1') then
+		case(STATE) is
+		
+			when IDLE =>
+				
+				
+			when AKTIV =>
+			
+		end case;
+	
+	
+	end if;
+
+end process;
+		
+
+end svga_master_arch;
+--	Design considerations --
+--	Enable on 0 to 1, not "stay at 1"
+-- States -- Off, idle, read.
+-- Implement Clock, divided, 50Mhz.
 
 --So VSYNC, while HSYNC, then HREF means data valid. In this case, we should have data_valid signal out.
 -- Hsync time - 1190 total, 80 Sync, 179 Front porch, 800 Href, 131 back porch.
